03faba5c738cf81c85de0cb33729394b08f3cf67
Arthur Brant <arthurcl.br@hotmail.com>
2021-12-07 18:01:25 -0300

    fixed module error in bubbleSort when running in node

--- sort/ts/bubbleSort.ts
+++ sort/ts/bubbleSort.ts
@@ -1,7 +1,7 @@
 const bubbleSort = (list: number[]): number[] => {
     let n = list.length, end = n;
     for (let i = 0; i < n - 1; i++) {
-        for (let k = 1; k < end - 1; k++) {
+        for (let k = 0; k < end - 1; k++) {
             if (list[k] > list[k+1]) {
                 let aux = list[k];
                 list[k] = list[k+1];
--- tests/ts/test_bubbleSort.ts
+++ tests/ts/test_bubbleSort.ts
@@ -1,11 +1,33 @@
 import  bubbleSort from '../../sort/ts/bubbleSort';
 
 function main(): void {
-    let list: number[] = (new Array(50000)).fill(0);
+    let list: number[] = (new Array(100000)).fill(0);
+
     // Fill the array with random numbers
-    list.map(() => Math.random() * 100000);
+    list.map((e, i) => list[i] = Math.round(Math.random() * 100000));
+
+    // Sort the list
+    console.time('sorting');
     list = bubbleSort(list);
-    console.log(list);
+    console.timeEnd('sorting');
+
+    logList(list);
+}
+
+const logList = function (l: number[]): void {
+    let size = l.length;
+    if (size <= 50) console.log(l)
+    else {
+        console.log("Elementos do inicio da lista")
+        for (let i = 0; i < 10; i++) {
+            console.log(l[i]);
+        }
+        
+        console.log("Elementos do fim da lista")
+        for (let i = size - 1; i > size - 10; i--) {
+            console.log(l[i]);
+        }
+    }
 }
 
 main();

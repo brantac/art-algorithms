b4f5f5272bbb843bf6f7d55395c332acaeace572
Arthur Brant <arthurcl.br@hotmail.com>
2021-12-09 14:25:28 -0300

    insertion sort test added and evaluated

--- tests/ts/test_bubbleSort.ts
+++ tests/ts/test_bubbleSort.ts
@@ -1,21 +1,30 @@
 import  bubbleSort from '../../sort/ts/bubbleSort';
+import insertionSort from '../../sort/ts/insertionSort';
 
 function main(): void {
-    let list: number[] = (new Array(100000)).fill(0);
+    let listB: number[] = (new Array(50000)).fill(0);
+    let listA: number[] = (new Array(50000)).fill(0);
 
-    // Fill the array with random numbers
-    list.map((e, i) => list[i] = Math.round(Math.random() * 100000));
+    // Fill the arrays with random numbers
+    listA.map((e, i) => listA[i] = Math.round(Math.random() * 100000));
+    listB.map((e, i) => listB[i] = Math.round(Math.random() * 100000));
 
-    // Sort the list
-    console.time('sorting');
-    list = bubbleSort(list);
-    console.timeEnd('sorting');
+    // Sort lists
+    console.time('sort-list-a')
+    bubbleSort(listA);
+    console.timeEnd('sort-list-a');
+    console.time('sort-list-b');
+    insertionSort(listB);
+    console.timeEnd('sort-list-b')
 
-    logList(list);
+    // Log lists
+    logList(listA, 'A');
+    logList(listB, 'B');
 }
 
-const logList = function (l: number[]): void {
+const logList = function (l: number[], name: string): void {
     let size = l.length;
+    console.log(`List ${name}`);
     if (size <= 50) console.log(l)
     else {
         console.log("Elementos do inicio da lista")
@@ -28,6 +37,26 @@ const logList = function (l: number[]): void {
             console.log(l[i]);
         }
     }
+}
+
+const isOrdered = (list: number[], ascending: boolean): boolean => {
+    let ordered = true;
+    let i = 0;
+
+    if (ascending) {
+        while (i <= list.length - 2 && ordered) {
+            if (list[i] > list[i+1]) ordered = false;
+            else i++;
+        };
+    }
+    else {
+        while (i <= list.length - 2 && ordered) {
+            if (list[i] < list[i + 1]) ordered = false;
+            else i--;
+        };
+    }
+    
+    return ordered;
 }
 
 main();
